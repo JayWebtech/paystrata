e47d128f84ae8f6e26d851929b08be03af4fa502
JayWebtech <jethroadamzy@gmail.com>
2025-08-22 16:06:49 +0100

    fixes

--- server/src/index.ts
+++ server/src/index.ts
@@ -63,7 +63,7 @@ const corsOptions = {
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'x-reference-code', 'x-transaction-hash']
 };
 
 // Middleware
